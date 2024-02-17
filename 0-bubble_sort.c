f42ed092a92164cce3ed7db18c3e1b153cd0692b
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-02-17 22:25:16 +0000

    a

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,42 @@
-#include "sort.h"
+#include <stdio.h>
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: pointer to the first element of the array to be sorted
+ * @size: number of elements in the array
+ * Description: This function implements the Bubble sort algorithm
+ * to sort an array of integers in ascending order. It iterates through
+ * the array multiple times, comparing adjacent
+ * elements and swapping them if they are in the wrong order.
+ * The process continues until no more swaps are needed, indicating
+ * that the array is sorted. After each swap, the array is printed to
+ * the standard output.
+ * Time complexity:
+ * - Best case: O(n) when the array is already sorted
+ * - Average case: O(n^2) when the array is in reverse order or randomly ordered
+ * - Worst case: O(n^2) when the array is in reverse order or randomly ordered
  */
 void bubble_sort(int *array, size_t size)
 {
+int i, j, temp;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
+for (i =   0; i < size -   1; i++)
+{
+for (j =   0; j < size - i -   1; j++)
+{
+if (array[j] > array[j +   1]) {
+/* Swap array[j] and array[j +   1] */
+temp = array[j];
+array[j] = array[j +   1];
+array[j +   1] = temp;
+/* Print the array after each swap */
+for (int k =   0; k < size; k++)
+{
+printf("%d ", array[k]);
 }
+printf("\n");
+}
+}
+}
+}
+
